2cc86a1a0e9618c834bd724d5572b23783246c7d
Shilpa Konatham <lkonatham@gmail.com>
2013-06-25 14:13:58 -0500

    updates to canvas.html 5

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,22 @@
-var result = function(temp) {
-                return(5/9)*(temp-32);
-             }
-
-};
-
-
+function apply()
+{
+   var a = tform.myoption.value;
+   if(a=="Fahrenheit")
+   {
+      var val=tform.txt.value;
+      var tf=parseInt(val);
+      var tc=(5/9)*(tf-32);
+      var res=Math.round(tc*Math.pow(10,2))/Math.pow(10,2);
+      tform.txt1.value=res+" C";
+    }
+    else
+    {
+       var val=tform.txt.value;
+       var tc=parseInt(val);
+       var tf=((9/5)*tc)+32;
+       var res=Math.round(tc*Math.pow(10,2))/Math.pow(10,2);
+       tform.txt1.value=res+" F";
+    }
 $j(document).ready(function() {
 
    $j('#output a').click(function() {
